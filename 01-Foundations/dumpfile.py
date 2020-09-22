91f9d4f0b027952f160c9831eebaed4578ea4aa4
Kayode Omotunde <kayomotunde@gmail.com>
2020-09-22 21:06:49 +0400

    added two lists to make one

--- 01-Foundations/dumpfile.py
+++ 01-Foundations/dumpfile.py
@@ -1,12 +1,18 @@
-a = [4, 2, 1, 3]
-i = 0
+# a = [4, 2, 1, 3]
+# i = 0
 
-while i < len(a):
-    min = i
-    for j in range(i, len(a)):
-        if a[j] < a[min]:
-            min = j
+# while i < len(a):
+#     min = i
+#     for j in range(i, len(a)):
+#         if a[j] < a[min]:
+#             min = j
             
-    a[i], a[min] = a[min], a[i]
-    i += 1
-print(a)
+#     a[i], a[min] = a[min], a[i]
+#     i += 1
+# print(a)
+
+# demonstrate the joining of two lists to one by adding them
+a = [1, 2]
+b = [3, 4]
+c = a + b
+print(c)
